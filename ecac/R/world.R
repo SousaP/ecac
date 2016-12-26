6d3b99f59a7c3f1b5276be0a2b95f67437d506aa
Mário Ferreira <ei12049@fe.up.pt>
2016-12-26 16:23:55 +0000

    ghjkl

--- ecac/R/world.R
+++ ecac/R/world.R
@@ -13,13 +13,20 @@ df_loan$rel <- (df_loan$payments * 100 / df_loan$`average salary`)
 df_loan$rel <- (df_loan$payments / df_loan$`average salary`)
 df_loan <- merge(df_loan, district[ , c("code", "unemploymant rate '96")], by.x = "district_id", by.y = "code")
 colnames(df_loan)[13] <- "rate"
+#Normallize with professor formula
+#subtracting the average and dividing the result by the sample standard deviation
+df_loan$norm_rel <- ((df_loan$rel - mean(df_loan$rel)) / (sd(df_loan$rel)) * sqrt((nrow(df_loan)-1)/nrow(df_loan)))
 # Normalize unemployment rate
 df_loan[13] <- lapply(df_loan[13], scale)
 #Another Aproach
 range01 <- function(x){(x-min(x))/(max(x)-min(x))}
 df_loan[13] <- range01(df_loan[13])
+df_loan$status <- format(round(df_loan$status, 2), nsmall = 2)
 
-grpLoan <- kmeans(df_loan[,c("status","rel")], centers=4)
+grpLoanNorm <- kmeans(df_loan[,c("norm_rel","status")], centers=4)
+grpLoanNorm
+
+grpLoan <- kmeans(df_loan[,c("rel","status")], centers=4)
 grpLoan
 
 grpDur <- kmeans(df_loan[,c("duration","status")], centers=3)
@@ -39,7 +46,3 @@ for (i in 2:25) wss[i] <- sum(kmeans(df_loan[,c("payments","status")],
                                      centers=i)$withinss)
 plot(1:25, wss, type="b", xlab="Number of Clusters",
      ylab="Within groups sum of squares")
-
-#Normallize with professor formula
-#subtracting the average and dividing the result by the sample standard deviation
-df_loan$norm_rel <- ((df_loan$rel - mean(df_loan$rel)) / (sd(df_loan$rel)) * sqrt((nrow(df_loan)-1)/nrow(df_loan)))
